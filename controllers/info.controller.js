42d9c0f2c42f218c3b2b3811a047bc597c6b1abf
AlexanderOcon <brandon.hermida.ocon@gmail.com>
2026-08-24 12:25:54 -0600

    initial commits

---
+++ controllers/info.controller.js
@@ -0,0 +1,8 @@
+import db from '../firebase.js';
+import supabase from '../supabase.js';
+
+export const informacion = async (req, res) => {
+  res.json({
+    mensaje: 'Esta es una funcion de prueba del Backend Grupal'
+  });
+};
